d28ab3a8388f6bfc1b3e426f86e95043fd5d730b
amit.sheffer <freakazoid85@gmail.com>
2020-06-24 15:39:35 +0300

    lesson-24

--- App.js
+++ App.js
@@ -4,13 +4,9 @@ import { View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
     <View style={{padding: 30}}>
-      <View>
-        <View>
-          <TextInput placeholder='Course Goal' style={{borderColor: 'black', borderWidth: 1, padding: 10}}/>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+          <TextInput placeholder='Course Goal' style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}/>
           <Button title='ADD' />
-        </View>
-        <View>
-        </View>
       </View>
     </View>
   );
