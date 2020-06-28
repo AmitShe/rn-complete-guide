54c1d58520e8e971fa25f07df8ac379d8c6beec5
amit.sheffer <freakazoid85@gmail.com>
2020-06-28 20:51:38 +0300

    add 'addGoalHandler'

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, Button } from 'react-native';
+import { StyleSheet, View, TextInput, Button, Text } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
   const goalInputHandler = (enterText) => {
     setEnteredGoal(enterText);
   };
 
   const addGoalHandler = () => {
-    console.log(enteredGoal)
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
   }
 
   return (
@@ -21,6 +22,9 @@ export default function App() {
         value={enteredGoal}
         />
         <Button title='ADD' onPress={addGoalHandler}/>
+      </View>
+      <View>
+        <Text value={courseGoals} />
       </View>
     </View>
   );
