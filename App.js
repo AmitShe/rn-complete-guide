55fc0c969e8c6b3eef8ced0912cb6139d237cc18
amit.sheffer <freakazoid85@gmail.com>
2020-07-08 20:46:54 +0300

    style change and remove import

--- App.js
+++ App.js
@@ -1,66 +1,59 @@
-import React, { useState } from "react";
-import {
-	StyleSheet,
-	View,
-	TextInput,
-	Button,
-	Text,
-	_ScrollView,
-} from "react-native";
+import React, { useState } from "react"
+import { StyleSheet, View, TextInput, Button, Text } from "react-native"
 
 export default function App() {
-	const [enteredGoal, setEnteredGoal] = useState("");
-	const [courseGoals, setCourseGoals] = useState([]);
-	const goalInputHandler = (enterText) => {
-		setEnteredGoal(enterText);
-	};
+  const [enteredGoal, setEnteredGoal] = useState("")
+  const [courseGoals, setCourseGoals] = useState([])
+  const goalInputHandler = (enterText) => {
+    setEnteredGoal(enterText)
+  }
 
-	const addGoalHandler = () => {
-		setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
-	};
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal])
+  }
 
-	return (
-		<View style={style.screen}>
-			<View style={style.inputComtainer}>
-				<TextInput
-					placeholder="Course Goal"
-					style={style.input}
-					onChangeText={goalInputHandler}
-					value={enteredGoal}
-				/>
-				<Button title="ADD" onPress={addGoalHandler} />
-			</View>
-			<View>
-				{courseGoals.map((goal) => (
-					<View key={goal} style={style.listItem}>
-						<Text>{goal}</Text>
-					</View>
-				))}
-			</View>
-		</View>
-	);
+  return (
+    <View style={style.screen}>
+      <View style={style.inputComtainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={style.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <View>
+        {courseGoals.map((goal) => (
+          <View key={goal} style={style.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  )
 }
 
 const style = StyleSheet.create({
-	screen: {
-		padding: 30,
-	},
-	inputComtainer: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		alignItems: "center",
-	},
-	input: {
-		width: "80%",
-		borderColor: "black",
-		borderWidth: 1,
-		padding: 10,
-	},
-	listItem: {
+  screen: {
+    padding: 30,
+  },
+  inputComtainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  listItem: {
     padding: 10,
     margin: 10,
-		backgroundColor: "#ccc",
-		borderColor: "black",
-		borderWidth: 1,
-	},
-});
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+})
